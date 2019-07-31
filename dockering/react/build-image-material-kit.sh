1b15546f1aa698dda31a067ffd1ad78a948d5aa1
mikuu <biaofu@thoughtworksc.om>
2019-07-31 15:14:26 +0800

    initial build

---
+++ dockering/react/build-image-material-kit.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker build --rm -f docker-files/material-kit/Dockerfile --tag=material-kit:0.0.1 .
--- dockering/react/build-image-vcer-ui.sh
+++ dockering/react/build-image-vcer-ui.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker build --rm -f Dockerfile --tag=sandbox-vcer-ui:0.0.1 .
+docker build --rm -f docker-files/vcer-ui/Dockerfile --tag=sandbox-vcer-ui:0.0.1 .
